de7a97f85665b3aba96c03201e5a3827251ee901
Paco Navarro <xe55471>
2017-06-17 14:01:02 +0200

    express config

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ var app = express();
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 4200;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
+var server_dispenser = !!process.env.TICKET_DISPENSER;
+
 app.listen(server_port, server_ip_address, function() {
   console.log('App listening on port ' + server_port);
 });
+
+if (server_dispenser) {
+  require('dispenser/index')(app);
+} else {
+  require('ticket-roll/index')(app);
+}
+
