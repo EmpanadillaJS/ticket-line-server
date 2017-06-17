70a52479386575bff27f45cceaa1d1dbbade1cad
msalvarez <msal.varez@gmail.com>
2017-06-17 19:44:06 +0200

    inicio del proceso de creacion de token anidando llamadas

--- dispenser/queue/number/index.js
+++ dispenser/queue/number/index.js
@@ -1,8 +1,42 @@
 module.exports = init;
+const rp = require('request-promise');
 
 function init(app) {
 
-  app.get('*/queue/number/*',  (req, res) => {
-    
-  });
+  let calls=[];
+
+
+  let resolverLlamadas=(auxCalls)=> function(parsedBody){
+    console.log(parsedBody);
+  };
+
+
+
+  let intervalFunction= ()=>{
+    if(calls.length>0){
+      let auxCalls=calls.slice();
+      calls=[];
+      let options={
+            method: 'POST',
+            uri: 'http://ticket-line-dev.herokuapp.com/event/queuePosition/',
+            body: {
+                length: auxCalls.length
+            },
+            json: true // Automatically stringifies the body to JSON
+        };
+
+      rp(getOtions())
+        .then(resolverLlamadas(auxCalls))
+        .catch(function (err) {console.error("error: perdidas "+length+" llamadas.");});
+    }
+  };
+
+
+
+
+  app.get('*/queue/number/*',  (request, response) => calls.push({request,response}));
+
+  setInterval(intervalFunction, 100);
+
+
 }
